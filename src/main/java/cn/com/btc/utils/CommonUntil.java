c651fc62e914a3901d17ce3c71c4486d5ba765ae
shaolp <slp1987>
2018-06-13 11:06:18 +0800

    init..............

--- src/main/java/cn/com/btc/utils/CommonUntil.java
+++ src/main/java/cn/com/btc/utils/CommonUntil.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public class CommonUntil {
     public static File binDir = new File(System.getProperty("user.dir")).getAbsoluteFile();
-//    public static File workingDir = binDir.getParentFile();
-        public static File workingDir = binDir;
+    public static File workingDir = binDir.getParentFile();
+//        public static File workingDir = binDir;
     public static File confDir = new File(workingDir, "conf");
     public static File dataDir = new File(workingDir, "data");
 }
